f93f40136ecda9d8048c44722bd8e8416bbac30d
Max Zawisa <maxrzaw@umich.edu>
2020-06-12 23:12:19 -0400

    Done for the night

--- api/serializers.py
+++ api/serializers.py
@@ -1,11 +1,10 @@
 from django.contrib.auth.models import Group
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
-User = get_user_model()
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
-        model = User
+        model = get_user_model()
         fields = ['url', 'username', 'email', 'group', 'bio', 'year',]
 
 
