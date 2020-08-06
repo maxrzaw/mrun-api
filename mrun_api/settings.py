56455395a81317e899d4d9db2b52ed95d6a3e38c
Max Zawisa <maxrzaw@umich.edu>
2020-08-06 00:01:32 +0000

    Changed for server

--- mrun_api/settings.py
+++ mrun_api/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'OPTIONS': {
-            'read_default_file': '/etc/my.cnf',
+            'read_default_file': '/etc/mysql/my.cnf',
         },
     }
 }
